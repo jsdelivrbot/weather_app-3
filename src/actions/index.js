0eb3456fffa71cd5f6e8a1e6b25af8ef94c74df6
lgants <lgants@gmail.com>
2017-01-07 17:02:00 -0600

    completed tests to ensure middelware promise function as intended

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,14 +4,14 @@ const API_KEY = 'a5f7912f8d9dd964a927fa256024e09b';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
 
 //created seperate variable so we can reuse this value in reducer
-export const FETCH_WEATHER = 'FETCH_WEATHER'
+export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city){
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
-
+  
   return {
     type: FETCH_WEATHER,
     payload: request
-  }
+  };
 }
--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import bindActionCreators from 'redux';
+import { bindActionCreators } from 'redux';
 import { fetchWeather } from '../actions/index'
 
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props){
     super(props);
 
     this.state = { term: ''};
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event){
     //event.target.value is not React; it's just vanilla JS
-    console.log(event.target.value)
     this.setState({ term: event.target.value})
 
   }
@@ -23,6 +23,9 @@ export default class SearchBar extends Component {
   //you always need to prevent default with forms in React
   onFormSubmit(event){
     event.preventDefault();
+
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: ''});
   }
 
   render(){
@@ -43,6 +46,14 @@ export default class SearchBar extends Component {
     )
   }
 }
-
-
 //button is controlled field (i.e. form element where value of the input is set by the state of the component)
+
+//bindActionCreators ensures that the dispatch flows to the reducers
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+//pass null as first argument because mapDispatchToProps must always be the second argument when used with connect
+export default connect(null, mapDispatchToProps)(SearchBar);
+
+//by binding action creator to fetch weather to dispatch and the mapping it to props, gives us access to the function this.props.fetchWeather inside the component
